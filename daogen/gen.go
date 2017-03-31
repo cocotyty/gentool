3f1e1c1d8b377290077500b3fcc8be4fa5b1dc63
tangyang <cocotyty@sina.com>
2017-03-31 17:34:43 +0800

    update

--- daogen/gen.go
+++ daogen/gen.go
@@ -212,7 +212,7 @@ func (dao *{{.name}}Dao)FindByID(ID int)(one *{{.name}},err error){
 }
 func (dao *{{.name}}Dao)Page(page ,pageSize int)(list []*{{.name}},err error){
 	list=[]*{{.name}}{}
-	err=dao.DB.Select(&list,"select * from ` + "`" + `{{.table}}` + "`" + ` limit ?,?",page*(pageSize-1),pageSize)
+	err=dao.DB.Select(&list,"select "+dao.Fields()+" from ` + "`" + `{{.table}}` + "`" + ` limit ?,?",page*(pageSize-1),pageSize)
 	if err!=nil && err!= sql.ErrNoRows {
 		return nil,err
 	}
@@ -221,7 +221,7 @@ func (dao *{{.name}}Dao)Page(page ,pageSize int)(list []*{{.name}},err error){
 func (dao *{{.name}}Dao)WherePage(whereSql string,page ,pageSize int,args ... interface{})(list []*{{.name}},err error){
 	list=[]*{{.name}}{}
 	args = append(args,page*(pageSize-1),pageSize)
-	err=dao.DB.Select(&list,"select * from ` + "`" + `{{.table}}` + "`" + ` where "+whereSql+" limit ?,?",args...)
+	err=dao.DB.Select(&list,"select "+dao.Fields()+" from ` + "`" + `{{.table}}` + "`" + ` where "+whereSql+" limit ?,?",args...)
 	if err!=nil && err!= sql.ErrNoRows {
 		return nil,err
 	}
